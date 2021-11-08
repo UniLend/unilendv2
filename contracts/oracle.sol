fe059f3e8588914ad070d7fded0d1ecc6b79d066
syzonehq <57961326+syzonehq@users.noreply.github.com>
2021-11-08 18:48:45 +0530

    removed uniswap v3 dummy
    removed dummy v3 price function

--- contracts/oracle.sol
+++ contracts/oracle.sol
@@ -53,52 +53,19 @@ contract UnilendV2oracle is Ownable {
         if(token0 == WETH && token1 != WETH){
             price0 = 1 ether;
             price1 = getChainLinkAssetPrice(token1);
-            
-            if (price1 <= 0) {
-                price1 = getUniswapV3Price(token0, token1);
-            }
         } 
         else if(token0 != WETH && token1 == WETH){
             price0 = getChainLinkAssetPrice(token0);
             price1 = 1 ether;
-            
-            if (price0 <= 0) {
-                price0 = getUniswapV3Price(token1, token0);
-            }
         } 
         else {
             price0 = getChainLinkAssetPrice(token0);
             price1 = getChainLinkAssetPrice(token1);
-            
-            if (price0 <= 0) {
-                price0 = getUniswapV3Price(token1, token0);
-            }
-
-            if (price1 <= 0) {
-                price1 = getUniswapV3Price(token0, token1);
-            }
         }
         
-        return (amount.mul(uint256(price1))).div(uint256(price0));
-    }
-    
-
-
-
-
-
-    // tmp xxxxxx 
-    function getUniswapV3Price(address token0, address token1) public view returns (int256) {
-        if(aPrice[token0][token1] > 0){
-            return int(aPrice[token0][token1]);
-        } else {
-            return 1 ether;
+        if(price0 > 0 && price1 > 0){
+            return (amount.mul(uint256(price1))).div(uint256(price0));
         }
-    }
-    
-    mapping(address => mapping(address => uint)) public aPrice;
-    function setTokenPrice(address token0, address token1, uint price) public {
-        aPrice[token0][token1] = price;
     }
     
 }
